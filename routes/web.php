f715756012477bbb54196e39b1f799494f537a33
MD Sakib Hasan <23-50316-1@student.aiub.edu>
2026-09-18 05:12:53 +0600

    Add friendly root welcome route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use App\Http\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->json([
+        'name' => 'TinyLink API',
+        'status' => 'online',
+        'message' => 'Welcome to TinyLink URL Shortener API',
+    ]);
+});
+
 // This route is intentionally public. It is the short link people will share.
 Route::get('/{shortCode}', RedirectController::class)
     ->where('shortCode', '[A-Za-z0-9-]+')
